0e186c2b27727e28e0c52966465e20b21a656edd
venomiele <atrandafir35@yahoo.com>
2021-12-24 01:33:59 -0800

    added logic for all of the tools

--- notes.js
+++ notes.js
@@ -2,15 +2,19 @@ let editBtn = document.querySelector(".edit");
 let deleteBtn = document.querySelector(".delete");
 let saveBtn = document.querySelector(".save");
 let editArea = document.querySelector(".main");
+let colorBtn = document.getElementById("create-color");
+let italicBtn = document.querySelector(".italic");
+let boldBtn = document.querySelector(".bold");
+let sizeUp = document.querySelector(".size-up");
+let sizeDown = document.querySelector(".size-down");
+let coloredTitle = document.querySelector(".colored");
 
+let currentSize = 1.25;
 
 //Get data
 function getData() {
     let getTextData = localStorage.getItem("myData");
     editArea.value = getTextData;
-  //  if(!getTextData === null) {
-        editArea.style.display = "block";
-            }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -54,3 +58,57 @@ saveBtn.addEventListener("click", () => {
     let savedData = editArea.value;
     localStorage.setItem("myData", savedData);
 });
+
+
+// Color Button
+
+colorBtn.addEventListener("input", () => {
+    let colored = colorBtn.value;
+    editArea.style.color = colored;
+})
+
+//Italic Btn
+function italicTxt() {
+    editArea.classList.toggle("italic-text");
+}
+
+italicBtn.addEventListener("click", () => {
+italicTxt();
+})
+
+// Bold Btn 
+function boldTxt() {
+    editArea.classList.toggle("bold-text");
+}
+
+boldBtn.addEventListener("click", () => {
+    boldTxt();
+    })
+
+    // Sizes buttons 
+
+
+
+function sizeUpTxt() {
+    editArea.style.fontSize = `${currentSize}rem`
+}
+
+function sizeDownTxt() {
+    editArea.style.fontSize = `${currentSize}rem`
+}
+
+    sizeUp.addEventListener("click", () => {
+        currentSize++;
+        sizeUpTxt();
+    })
+
+    sizeDown.addEventListener("click", () => {
+        currentSize--;
+        sizeDownTxt();
+    });
+
+    // Title live edit
+
+function coloredTitle() {
+
+}
